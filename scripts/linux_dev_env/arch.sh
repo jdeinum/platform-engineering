6ee2903edd3c7e33a8c4f62a0350936f92ee52b5
Jacob Deinum <jdeinum@nullspaces.org>
2025-05-26 09:50:24 -0600

    deleted arch dockerfile, debian should be good enough

--- scripts/linux_dev_env/arch.sh
+++ scripts/linux_dev_env/arch.sh
@@ -1,12 +1,12 @@
 install_required_packages() {
   # update the package repositories
   # sudo pacman -Syu 
-  pacman -Syu 
+  pacman -Syu  --noconfirm
 
   # first thing we'll do is install the required packages for yay
   # an AUR helper that makes managing the AUR a bit easier
   if ! command -v yay >/dev/null 2>&1; then
-    pacman -S --needed git base-devel && \
+    pacman -S --needed git base-devel  --noconfirm && \
       git clone https://aur.archlinux.org/yay.git && \
       cd yay && makepkg -si
   fi
