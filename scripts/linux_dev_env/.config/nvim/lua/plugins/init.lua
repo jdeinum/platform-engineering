fecd4c540e2d001cfb5d214f94979f9f69651b59
Jacob Deinum <jdeinum@nullspaces.org>
2025-07-21 21:18:04 -0600

    added lazvim

--- scripts/linux_dev_env/.config/nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-	require("plugins.ui"),
-	require("plugins.misc"),
-	require("plugins.editor"),
-	require("plugins.lsp"),
-	require("plugins.completion"),
-	require("plugins.git"),
-	require("plugins.navigation"),
-}
